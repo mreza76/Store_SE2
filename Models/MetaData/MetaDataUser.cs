42034cffcb8c726912608e67d985dc2701e2b2d7
mrz <chinot232@gmail.com>
2019-05-19 11:28:57 +0430

    models configuration

---
+++ Models/MetaData/MetaDataUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Storenarm2.Models.Domins
+{
+    internal class MetaDataUser
+    {
+      
+    }
+
+    [MetadataType(typeof(MetaDataUser))]
+    public partial class Tbl_User
+    {
+
+    }
+
+}
